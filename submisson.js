47f40277d760354fcac64f7d68500d53829bad23
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-05-26 16:00:54 -0400

    created removeScores fx

--- submisson.js
+++ submisson.js
@@ -1,13 +1,31 @@
 function showScoreBoard() {
     // gets score from storage or adds score to empty array
-    var 
+    var scores = JSON.parse(window.localStorage.getItem("scores")) || [];
+
+    // arrange scores in descending order
+    highscores.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+    scores.forEach(function(score) {
+        //li tag for each score
+        var liTag = document.createElement("li");
+        liTag.textContent = score,initials + " - " + score.score;
+
+        // display on screen
+        var olEL = document.getElementById("scores");
+        olEL.appendChild(liTag);
+    })
 }
 
-// function showResponse(event) {
-//     var response =
-//     "thank you for your completion " + "nameSubmit.value";
-//     submitResponseEl.text.Content = response; 
+function removeScores() {
+    window.localStorage.removeItem("scores");
+    window.location.reload();
+}
+document.getElementById("Clear All").onclick = removeScores;
 
-// };
+// run function when page loads
+showScoreBoard();
+
 
 
